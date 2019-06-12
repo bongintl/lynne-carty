ba98ed0cd673512437688f3f22724b00309c80ef
Ubuntu <ubuntu@ip-172-31-10-88.us-east-2.compute.internal>
2019-06-12 23:16:30 +0000

    back to triangle

--- src/gl/renderer.js
+++ src/gl/renderer.js
@@ -6,8 +6,7 @@ var vert = `
     precision highp float;
     attribute vec2 position;
     void main () {
-        vec2 p = mix( offset, offset + size, position * .5 + .5 );
-        gl_Position = vec4( p * 2. - 1., 0., 1. );
+        gl_Position = vec4( position, 0., 1. );
     }
 `
 
@@ -17,13 +16,10 @@ module.exports = ( gl, frag ) => {
 
     var position = createBuffer( gl, new Float32Array([
         -1, -1,
-        -1, 1,
-        1, 1,
-        -1, -1,
-        1, 1,
-        1, -1
+        -1, 3,
+        3, -1
     ]))
-    var geometry = createVAO( gl, [{
+    var triangle = createVAO( gl, [{
         buffer: position,
         type: gl.FLOAT,
         size: 2
@@ -31,20 +27,15 @@ module.exports = ( gl, frag ) => {
     
     gl.enable( gl.BLEND );
     gl.disable( gl.DEPTH_TEST );
-    gl.blendEquation( gl.FUNC_ADD );
-    gl.blendFunc( gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA );
     
     shader.bind();
-    geometry.bind();
-    
-    shader.uniforms.offset = [ 0, 0 ];
-    shader.uniforms.size = [ 1, 1 ];
+    triangle.bind();
     
     return {
         uniforms: shader.uniforms,
         clear: () => gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT ),
         draw: () => {
-            gl.drawArrays( gl.TRIANGLES, 0, 6 );
-        },
+            gl.drawArrays( gl.TRIANGLES, 0, 3 );
+        }
     }
 }
