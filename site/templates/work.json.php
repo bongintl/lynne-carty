3e7b1604b4bca69086ee5005e013a715f3f86a21
Ubuntu <ubuntu@ip-172-31-10-88.us-east-2.compute.internal>
2019-06-13 13:11:10 +0000

    its a kirby now

---
+++ site/templates/work.json.php
@@ -0,0 +1,20 @@
+<?php
+
+echo json_encode(
+    array_values(
+        $pages
+            -> find( 'work' )
+            -> children()
+            -> published()
+            -> flip()
+            -> toArray( function ( $project ) {
+                return [
+                    'title' => (string) $project -> title(),
+                    'url' => (string) $project -> url(),
+                    'thumbnail' => $project -> thumbnail() -> isNotEmpty()
+                        ? $project -> thumbnail() -> toFile() -> resize( 200 ) -> url()
+                        : null
+                ];
+            })
+    )
+);
