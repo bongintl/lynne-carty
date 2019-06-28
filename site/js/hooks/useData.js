cdac10c3ccaa777fdd3a13ecca85b8f0cc2aeb0b
Ubuntu <ubuntu@ip-172-31-10-88.us-east-2.compute.internal>
2019-06-28 09:05:35 +0000

    really now

--- site/js/hooks/useData.js
+++ site/js/hooks/useData.js
@@ -1,13 +1,25 @@
 import { useState, useEffect, useMemo } from 'react';
 import useFetch from './useFetch';
+import getColor from '~/utils/getColor';
 import tinycolor from 'tinycolor2';
 
 export default () => {
     var data = useFetch( './home.json' );
+    var [ projectColors, setProjectColors ] = useState( null );
+    useEffect( () => {
+        if ( !data ) return;
+        Promise.all( data.projects
+            .map( project => getColor( project.pixel ) )
+        ).then( setProjectColors )
+    }, [ data ] )
     return useMemo(() => {
-        if ( data === null ) return null;
+        if ( data === null || projectColors === null ) return null;
         var { projects, tags } = data;
-        projects = projects.map( ( project, i ) => ({ ...project, i }) )
+        projects = projects.map( ( project, i ) => ({
+            ...project,
+            i,
+            color: projectColors[ i ]
+        }))
         return {
             projects,
             tags: tags.map( ({ name }) => name ),
@@ -23,5 +35,5 @@ export default () => {
                 tinycolor( tag.color )
             ]))
         }
-    }, [ data ] );
+    }, [ data, projectColors ] );
 }
