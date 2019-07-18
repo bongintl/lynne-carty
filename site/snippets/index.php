93d715aa466ac72326b319718f0c9fe86cfe39ad
Ubuntu <ubuntu@ip-172-31-10-88.us-east-2.compute.internal>
2019-07-18 11:43:07 +0000

    absoluter urls

--- site/snippets/index.php
+++ site/snippets/index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./assets/main.css">
+    <link rel="stylesheet" href="<?= $kirby -> url( 'assets' ) ?>/main.css">
     <title><?= $site -> title() -> html() ?></title>
     <base href="<?= $site -> url() ?>">
     <?php snippet( 'meta', [
@@ -20,6 +20,6 @@
 </head>
 <body>
     <main></main>
-    <script src="./assets/main.js"></script>
+    <script src="<?= $kirby -> url( 'assets' ) ?>/main.js"></script>
 </body>
 </html>
